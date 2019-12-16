bf00dcaa813bfdf03618e6fd8f8957b8de44d566
Nathaniel P Maile <npmaile@gmail.com>
2019-12-16 13:49:18 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -l
 EXCLUSIONS=${INPUT_EXCLUSIONS:-'^$'}
 REGEX=${INPUT_FIND_REGEX:-'.*\.sh'}
+pwd
+ls -la
 cat "$(find . -regex "${REGEX}")"
 shellcheck "$(find . -regex "${REGEX}" | grep -v "${EXCLUSIONS}")"
