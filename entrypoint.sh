27ba33f8af0541981a34a842323744df40235579
Nathaniel P Maile <nathaniel_p_maile@homedepot.com>
2019-12-18 10:38:19 -0500

    message

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ EXCLUSIONS=${INPUT_EXCLUSIONS:-'^$'}
 REGEX=${INPUT_FIND_REGEX:-'.*\.sh'}
 pwd
 echo "checking files:"
-files="$(find . -regex "${REGEX}| grep -v "${EXCLUSIONS}")"
+files="$(find . -regex "${REGEX}"| grep -v "${EXCLUSIONS}")"
 for file in $files; do
 shellcheck $file
 done
